17f1ee3ba020ce7749d2d2d088e5c08e92cd323d
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 19:21:51 +0100

    chore: create campaigns db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Campaign.create(name: Faker::Company.name,
+                  percent_raised: rand(100),
+                  target_amount: rand(100_000_000),
+                  sector: Faker::Commerce.department(max: 1),
+                  country: Faker::Address.country,
+                  investment_multiple: rand(50_000))
+end
