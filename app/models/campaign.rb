bcc98234a1009ca98fec5a1ba0405f4f59971c91
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 18:46:27 +0100

    feat: campaign's name, sector and country can't be longer than 256 characters

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,4 +4,5 @@ class Campaign < ApplicationRecord
   validates :name, :target_amount, :investment_multiple, presence: true
   validates :target_amount, numericality: { less_than: 1_000_000_000.00 }
   validates :investment_multiple, numericality: { less_than: 500_000_000.00 }
+  validates :name, :sector, :country, length: { maximum: 256 }
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:target_amount) }
   it { is_expected.to validate_presence_of(:investment_multiple) }
+
   it { is_expected.to validate_numericality_of(:target_amount).is_less_than(1_000_000_000.00) }
   it { is_expected.to validate_numericality_of(:investment_multiple).is_less_than(500_000_000.00) }
+
+  it { is_expected.to validate_length_of(:name).is_at_most(256) }
+  it { is_expected.to validate_length_of(:sector).is_at_most(256) }
+  it { is_expected.to validate_length_of(:country).is_at_most(256) }
 end
