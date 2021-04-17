b1294dc7e4c1bf6cd09aa4d680bd80e7d1c26bca
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 18:42:20 +0100

    feat: investment multiple can be max of half of target amount

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,5 @@
 class Campaign < ApplicationRecord
   validates :name, :target_amount, :investment_multiple, presence: true
   validates :target_amount, numericality: { less_than: 1_000_000_000.00 }
+  validates :investment_multiple, numericality: { less_than: 500_000_000.00 }
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of(:target_amount) }
   it { is_expected.to validate_presence_of(:investment_multiple) }
   it { is_expected.to validate_numericality_of(:target_amount).is_less_than(1_000_000_000.00) }
+  it { is_expected.to validate_numericality_of(:investment_multiple).is_less_than(500_000_000.00) }
 end
