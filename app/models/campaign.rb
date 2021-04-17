eab9a19ebabfd81db2ab15a74a6867545504f42a
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 18:38:25 +0100

    feat: campaign's target amount can't be bigger than one billion

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,5 @@
 
 class Campaign < ApplicationRecord
   validates :name, :target_amount, :investment_multiple, presence: true
+  validates :target_amount, numericality: { less_than: 1_000_000_000.00 }
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:target_amount) }
   it { is_expected.to validate_presence_of(:investment_multiple) }
+  it { is_expected.to validate_numericality_of(:target_amount).is_less_than(1_000_000_000.00) }
 end
