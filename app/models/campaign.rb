af1f288dc27328ec700e1d93e529dbc7fc834c4e
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 18:19:40 +0100

    feat: validate presence of campaign's target amount

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Campaign < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :target_amount, presence: true
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,4 +2,5 @@
 
 RSpec.describe Campaign, type: :model do
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:target_amount) }
 end
