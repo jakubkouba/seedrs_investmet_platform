9efb98c321b5e47d8166372c8add6744a4f54754
Jakub Adler <adler.jakub@gmail.com>
2021-04-17 23:30:46 +0100

    feat: investment is invalid when invested amount is not multiple of investment multiple

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,4 +2,20 @@ class Investment < ApplicationRecord
   belongs_to :campaign
 
   validates :amount, :campaign, presence: true
+  validate :investment_multiple
+
+
+  private
+
+  def investment_multiple
+    message = 'Invested amount in not a multiple of investment multiple'
+    errors.add(:investment_multiple, message) unless investment_multiple?
+  end
+
+  def investment_multiple?
+    return false unless campaign_id
+
+    campaign = Campaign.find(campaign_id)
+    (amount % campaign.investment_multiple).zero?
+  end
 end
--- spec/models/investment_spec.rb
+++ spec/models/investment_spec.rb
@@ -1,6 +1,17 @@
 RSpec.describe Investment, type: :model do
+
   it { is_expected.to validate_presence_of :amount }
   it { is_expected.to validate_presence_of :campaign }
 
   it { is_expected.to belong_to :campaign }
+
+  describe 'when amount is not multiple of investment multiple' do
+    let(:campaign) { create(:campaign, investment_multiple: 50.00) }
+    let(:investment) { described_class.new(amount: 40.00, campaign: campaign) }
+
+    it 'is invalid' do
+      expect(investment.invalid?).to be true
+      expect(investment.errors.messages[:investment_multiple].first).to eq 'Invested amount in not a multiple of investment multiple'
+    end
+  end
 end
