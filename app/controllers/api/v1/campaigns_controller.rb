54e1d3da9e1a1c19619f513444334757c20e82ff
Jakub Adler <adler.jakub@gmail.com>
2021-04-18 21:10:32 +0100

    fix: permit pagination params

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -6,10 +6,14 @@ module Api
       def index
         return render json: {}, status: 404 if invalid_search_params?
 
-        render json: Campaign.filter_by(search_params).page(params[:page]).per(params[:per_page])
+        render json: Campaign.filter_by(search_params).page(pagination_params[:page]).per(pagination_params[:per_page])
       end
 
       private
+
+      def pagination_params
+        params.permit(:page, :per_page)
+      end
 
       def search_params
         params.permit(search: %i[name sector country investments_count])[:search]
