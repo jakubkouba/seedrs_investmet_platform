4ac612ee4fb06b14032ed24574d637b7db7d3e2f
Jakub Adler <adler.jakub@gmail.com>
2021-04-18 18:08:15 +0100

    feat: filter campaigns by attributes

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -4,7 +4,21 @@ module Api
   module V1
     class CampaignsController < ApplicationController
       def index
-        render json: Campaign.page(params[:page]).per(params[:per_page])
+        return render json: {}, status: 404 if invalid_search_params?
+
+        render json: Campaign.filter_by(search_params).page(params[:page]).per(params[:per_page])
+      end
+
+      private
+
+      def search_params
+        params.permit(search: %i[name sector country investments_count])[:search]
+      end
+
+      def invalid_search_params?
+        return false unless params[:search]
+
+        true if (params[:search].keys & search_params.keys).empty?
       end
     end
   end
--- spec/requests/api/v1/campaings_spec.rb
+++ spec/requests/api/v1/campaings_spec.rb
@@ -26,5 +26,39 @@ RSpec.describe Api::V1::CampaignsController, type: :request do
         end
       end
     end
+
+    context 'when having two campaigns from which one is from Health sector' do
+      let!(:campaign_from_health_sector) { create(:campaign, name: 'Campaign from Health sector', sector: 'Health') }
+      let!(:first_campaign) { create(:campaign, sector: 'Non-Health-sector') }
+
+      context 'when search by sector' do
+        let(:params) { { page: 1, per_page: 2, search: { sector: 'Health' } } }
+        let(:returned_campaigns) { JSON.parse(response.body) }
+
+        it 'returns campaign from Health sector' do
+          campaigns
+
+          expect(returned_campaigns.count).to eq 1
+          expect(returned_campaigns.first['name']).to eq 'Campaign from Health sector'
+        end
+
+      end
+
+      context 'when search by not permitted attribute' do
+        let(:params) { { page: 1, per_page: 2, search: { amount: 10 } } }
+        let(:returned_campaigns) { JSON.parse(response.body) }
+
+        before { campaigns }
+
+        it 'returns 404 response' do
+          expect(response).to have_http_status 404
+        end
+
+        it 'does not return any campaign' do
+          expect(returned_campaigns.count).to eq 0
+        end
+
+      end
+    end
   end
 end
